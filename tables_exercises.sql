3ccd074e901855df7dfb7c7226cc206d7c8a2342
Mark Foster <markdouglasfoster@gmail.com>
2022-09-07 14:07:13 -0500

    updated answers for the exercises

--- tables_exercises.sql
+++ tables_exercises.sql
@@ -2,11 +2,13 @@ USE employees;
 SHOW tables;
 DESCRIBE employees;
 /* 6. Which table(s) do you think contain a numeric type column?
+	All of them
 7. Which table(s) do you think contain a string type column?
+	departments, employees, titles
 8. Which table(s) do you think contain a date type column?
-		
+	dept_emp, dept_manager, employees, salaries, titles
 9. What is the relationship between the employees and the departments tables?
-		No relationship*/
+	No relationship*/
 
 SHOW CREATE TABLE dept_manager;
 
