29530f349de6c4d398f31a75d7297fea057d26a2
Mark Foster <markdouglasfoster@gmail.com>
2022-09-07 13:58:47 -0500

    prep for in-class exercises

---
+++ tables_exercises.sql
@@ -0,0 +1,12 @@
+USE employees;
+SHOW tables;
+DESCRIBE employees;
+/* 6. Which table(s) do you think contain a numeric type column?
+7. Which table(s) do you think contain a string type column?
+8. Which table(s) do you think contain a date type column?
+		
+9. What is the relationship between the employees and the departments tables?
+		No relationship*/
+
+SHOW CREATE TABLE dept_manager;
+
