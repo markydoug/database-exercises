426561f0ae5cf1e776000e7dc57710be9e2d5ca4
Mark Foster <markdouglasfoster@gmail.com>
2022-09-08 16:32:40 -0500

    end of day push

---
+++ functions_exercises.sql
@@ -0,0 +1,31 @@
+USE employees;
+
+-- 2. Write a query to to find all employees whose last name starts and ends with 'E'. Use concat() to combine their first and last name together as a single column named full_name.
+SELECT *, CONCAT(first_name, ' ', last_name) AS full_name
+FROM employees
+WHERE last_name LIKE "E%e";
+
+-- 3. Convert the names produced in your last query to all uppercase.
+SELECT *, UPPER(CONCAT(first_name, ' ', last_name)) AS full_name
+FROM employees
+WHERE last_name LIKE "E%e";
+
+-- 4. Find all employees hired in the 90s and born on Christmas. Use datediff() function to find how many days they have been working at the company (Hint: You will also need to use NOW() or CURDATE()),
+SELECT *,
+ DATEDIFF(CURDATE(), hire_date) AS Days_of_Employment
+FROM employees
+WHERE hire_date LIKE "199%" AND birth_date LIKE "%-12-25";
+
+-- 5. Find the smallest and largest current salary from the salaries table.
+SELECT MIN(salary), MAX(salary)
+FROM salaries;
+
+-- 6. Use your knowledge of built in SQL functions to generate a username for all of the employees. A username should be all lowercase, and consist of the first character of the employees first name, the first 4 characters of the employees last name, an underscore, the month the employee was born, and the last two digits of the year that they were born.
+SELECT CONCAT(
+    LOWER(SUBSTR(first_name, 1, 1)), 
+    LOWER(SUBSTR(last_name, 1, 4)),
+    '_',
+    SUBSTR(birth_date, 6, 2),
+    SUBSTR(birth_date, 3, 2))
+    AS username, first_name, last_name, birth_date
+FROM employees;
--- limit_exercises.sql
+++ limit_exercises.sql
@@ -3,18 +3,18 @@ USE employees;
 -- 2. List the first 10 distinct last name sorted in descending order.
 SELECT DISTINCT last_name
 FROM employees
-ORDER BY last_name
+ORDER BY last_name DESC
 LIMIT 10;
-/* Aamodt
-Acton
-Adachi
-Aingworth
-Akaboshi
-Akazan
-Akiyama
-Alameldin
-Albarhamtoshy
-Alblas*/
+/* 'Zykh'
+'Zyda'
+'Zwicker'
+'Zweizig'
+'Zumaque'
+'Zultner'
+'Zucker'
+'Zuberek'
+'Zschoche'
+'Zongker'*/
 
 -- 3. Find all previous or current employees hired in the 90s and born on Christmas. Find the first 5 employees hired in the 90's by sorting by hire date and limiting your results to the first 5 records.
 SELECT *
