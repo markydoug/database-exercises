3ec181b8db877460ca72e9e707ff1e8592f809f2
Mark Foster <markdouglasfoster@gmail.com>
2022-09-09 12:27:39 -0500

    Add exercises up to 3

--- join_exercises.sql
+++ join_exercises.sql
@@ -29,5 +29,48 @@ FROM roles
 LEFT JOIN users ON roles.id = users.role_id
 GROUP BY roles.name;
 
+/************Employees Database**************/
+USE employees;
 
+-- 2. Write a query that shows each department along with the name of the current manager for that department.
+SELECT d.dept_name AS Department_Name, 
+	CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager
+FROM departments AS d
+JOIN dept_manager AS dm
+    ON dm.dept_no = d.dept_no
+JOIN employees AS e
+    ON e.emp_no = dm.emp_no
+WHERE dm.to_date > CURDATE();
+
+-- 3. Find the name of all departments currently managed by women.
+SELECT d.dept_name AS Department_Name, 
+	CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager
+FROM departments AS d
+JOIN dept_manager AS dm
+    ON dm.dept_no = d.dept_no
+JOIN employees AS e
+    ON e.emp_no = dm.emp_no
+WHERE dm.to_date > CURDATE()
+    AND e.gender = 'F';
+
+-- 4. Find the current titles of employees currently working in the Customer Service department.
+
+
+-- 5. Find the current salary of all current managers.
+
+-- 6. Find the number of current employees in each department.
+
+-- 7. Which department has the highest average salary? Hint: Use current not historic information.
+
+-- 8. Who is the highest paid employee in the Marketing department?
+
+-- 9. Which current department manager has the highest salary?
+
+-- 10. Determine the average salary for each department. Use all salary information and round your results.
+
+
+/**************BONUS****************/
+-- 11. Find the names of all current employees, their department name, and their current manager's name.
+
+-- 12. Who is the highest paid employee within each department.
 
