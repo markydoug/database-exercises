0b764a6ac04d08d0fc72c22214bf5acb7e041373
Mark Foster <markdouglasfoster@gmail.com>
2022-09-09 12:27:01 -0500

    Update exercise 7 and 8

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -42,33 +42,47 @@ WHERE last_name LIKE "%q%" AND last_name NOT LIKE "%qu%"
 GROUP BY last_name;
 
 -- 7. Find all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
+-- Group by gender and first name
 SELECT first_name, gender, 
     COUNT(*)
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY first_name, gender;
 
+-- Group ONLY by gender
+SELECT gender, 
+    COUNT(*)
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya')
+GROUP BY gender;
+
 -- 8. Using your query that generates a username for all of the employees, generate a count employees for each unique username. 
-SELECT DISTINCT CONCAT(
-    LOWER(SUBSTR(first_name, 1, 1)), 
-    LOWER(SUBSTR(last_name, 1, 4)),
-    '_',
-    SUBSTR(birth_date, 6, 2),
-    SUBSTR(birth_date, 3, 2))
-    AS username, COUNT(*)
+SELECT 
+    LOWER(
+        CONCAT(
+            SUBSTR(first_name, 1, 1), 
+            SUBSTR(last_name, 1, 4),
+            '_',
+            SUBSTR(birth_date, 6, 2),
+            SUBSTR(birth_date, 3, 2)
+            ) 
+        ) AS username, COUNT(*)
 FROM employees
 GROUP BY username;
 -- Are there any duplicate usernames? YES
 -- BONUS: How many duplicate usernames are there? 13251
-SELECT DISTINCT CONCAT(
-    LOWER(SUBSTR(first_name, 1, 1)), 
-    LOWER(SUBSTR(last_name, 1, 4)),
-    '_',
-    SUBSTR(birth_date, 6, 2),
-    SUBSTR(birth_date, 3, 2))
-    AS username, COUNT(*) AS Total
+SELECT 
+    LOWER(
+        CONCAT(
+            SUBSTR(first_name, 1, 1), 
+            SUBSTR(last_name, 1, 4),
+            '_',
+            SUBSTR(birth_date, 6, 2),
+            SUBSTR(birth_date, 3, 2)
+            ) 
+        ) AS username, COUNT(*)
 FROM employees
-GROUP BY username
+GROUP BY username;
 HAVING Total > 1;
 
 /****************Bonus*****************/
