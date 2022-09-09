0c84aca5e2e99f2bcdde2a22a25f10d601eb55ad
Mark Foster <markdouglasfoster@gmail.com>
2022-09-09 15:03:56 -0500

    Add exercises up to bonus

--- join_exercises.sql
+++ join_exercises.sql
@@ -54,20 +54,105 @@ WHERE dm.to_date > CURDATE()
     AND e.gender = 'F';
 
 -- 4. Find the current titles of employees currently working in the Customer Service department.
-
+SELECT t.title, 
+    COUNT(de.emp_no) AS Count
+FROM titles as t
+JOIN employees AS e
+    ON e.emp_no = t.emp_no
+JOIN dept_emp AS de
+	ON de.emp_no = e.emp_no
+JOIN departments AS d
+	ON d.dept_no = de.dept_no
+WHERE t.to_date > CURDATE()
+	AND	de.to_date > CURDATE()
+	AND d.dept_name = "Customer Service"
+GROUP BY t.title
+ORDER BY t.title;
 
 -- 5. Find the current salary of all current managers.
+SELECT d.dept_name AS Department_Name, 
+	CONCAT(e.first_name, ' ', e.last_name) AS Department_Manager,
+	s.salary
+FROM departments AS d
+JOIN dept_manager AS dm
+    ON dm.dept_no = d.dept_no
+JOIN employees AS e
+    ON e.emp_no = dm.emp_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+WHERE dm.to_date > CURDATE()
+	AND s.to_date > CURDATE()
+ORDER BY Department_Name;
 
 -- 6. Find the number of current employees in each department.
+SELECT d.dept_no,
+	d.dept_name, 
+    COUNT(de.emp_no) AS num_employees
+FROM employees AS e
+JOIN dept_emp AS de
+	ON de.emp_no = e.emp_no
+JOIN departments AS d
+	ON d.dept_no = de.dept_no
+WHERE de.to_date > CURDATE()
+GROUP BY d.dept_no, d.dept_name
+ORDER BY d.dept_no;
 
 -- 7. Which department has the highest average salary? Hint: Use current not historic information.
+SELECT d.dept_name AS department_name, 
+	AVG(s.salary) AS average_salary
+FROM departments AS d
+JOIN dept_emp AS de
+    ON de.dept_no = d.dept_no
+JOIN employees AS e
+    ON e.emp_no = de.emp_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+WHERE s.to_date > CURDATE()
+GROUP BY Department_Name;
+ORDER BY s.salary DESC
 
 -- 8. Who is the highest paid employee in the Marketing department?
+SELECT e.first_name,
+	e.last_name
+FROM employees AS e
+JOIN dept_emp AS de
+    ON de.emp_no = e.emp_no
+JOIN departments AS d
+    ON d.dept_no = de.dept_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+WHERE s.to_date > CURDATE()
+	AND d.dept_name = "Marketing"
+ORDER BY s.salary DESC;
 
 -- 9. Which current department manager has the highest salary?
+SELECT  e.first_name, 
+    e.last_name,
+	s.salary,
+    d.dept_name
+FROM departments AS d
+JOIN dept_manager AS dm
+    ON dm.dept_no = d.dept_no
+JOIN employees AS e
+    ON e.emp_no = dm.emp_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+WHERE dm.to_date > CURDATE()
+	AND s.to_date > CURDATE()
+ORDER BY s.salary DESC;
 
 -- 10. Determine the average salary for each department. Use all salary information and round your results.
-
+SELECT d.dept_name AS department_name, 
+	ROUND(AVG(s.salary),0) AS average_salary
+FROM departments AS d
+JOIN dept_emp AS de
+    ON de.dept_no = d.dept_no
+JOIN employees AS e
+    ON e.emp_no = de.emp_no
+JOIN salaries AS s
+	ON s.emp_no = e.emp_no
+GROUP BY Department_Name
+ORDER BY average_salary DESC;
 
 /**************BONUS****************/
 -- 11. Find the names of all current employees, their department name, and their current manager's name.
