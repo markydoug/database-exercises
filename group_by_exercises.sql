f74b4cad75696b7d453b826ac64c77e73142ea82
Mark Foster <markdouglasfoster@gmail.com>
2022-09-09 10:09:51 -0500

    Add bonus exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -35,13 +35,15 @@ GROUP BY last_name;
 -- Qiwen
 
 -- 6. Add a COUNT() to your results (the query above) to find the number of employees with the same last name.
-SELECT last_name, COUNT(last_name)
+SELECT last_name, 
+    COUNT(last_name)
 FROM employees
 WHERE last_name LIKE "%q%" AND last_name NOT LIKE "%qu%"
 GROUP BY last_name;
 
 -- 7. Find all employees with first names 'Irena', 'Vidya', or 'Maya'. Use COUNT(*) and GROUP BY to find the number of employees for each gender with those names.
-SELECT first_name, gender, COUNT(*)
+SELECT first_name, gender, 
+    COUNT(*)
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY first_name, gender;
@@ -69,3 +71,54 @@ FROM employees
 GROUP BY username
 HAVING Total > 1;
 
+/****************Bonus*****************/
+-- Determine the historic average salary for each employee. 
+SELECT emp_no, 
+    ROUND(AVG(salary),2)
+FROM salaries
+GROUP BY emp_no;
+
+-- Using the dept_emp table, count how many current employees work in each department. The query result should show 9 rows, one for each department and the employee count.
+SELECT dept_no, 
+    COUNT(emp_no)
+FROM dept_emp
+WHERE to_date > CURDATE()
+GROUP BY dept_no;
+
+-- Determine how many different salaries each employee has had. This includes both historic and current.
+SELECT emp_no,
+	COUNT(salary)
+FROM salaries
+GROUP BY emp_no;
+
+-- Find the maximum salary for each employee.
+SELECT emp_no,
+	MAX(salary) AS Maximum_Salary
+FROM salaries
+GROUP BY emp_no;
+
+-- Find the minimum salary for each employee.
+SELECT emp_no,
+	MIN(salary) AS Minimum_Salary
+FROM salaries
+GROUP BY emp_no;
+
+-- Find the standard deviation of salaries for each employee.
+SELECT emp_no,
+	STDDEV(salary) AS Standard_Deviation_Salary
+FROM salaries
+GROUP BY emp_no;
+
+-- Now find the max salary for each employee where that max salary is greater than $150,000.
+SELECT emp_no,
+	MAX(salary) AS Maximum_Salary
+FROM salaries
+GROUP BY emp_no
+HAVING Maximum_Salary > 150000;
+
+-- Find the average salary for each employee where that average salary is between $80k and $90k.
+SELECT emp_no,
+	AVG(salary) AS Average_Salary
+FROM salaries
+GROUP BY emp_no
+HAVING Average_Salary BETWEEN 80000 AND 90000;
